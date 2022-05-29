ede180d1205e6c35c0ad87526ae0ddbd49a697c3
jesussantana <jesus@esmarketingdigital.com>
2022-05-29 08:54:12 +0200

    Project Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data analytics is the collection, transformation, and organization of data to draw conclusions, make predictions, and drive informed decision making.',
+    author='Jesus Santana',
+    license='MIT',
+)
